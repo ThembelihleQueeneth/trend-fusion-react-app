0c00d16d7573ee4299287556fbeaa6afa54da3a1
ThembelihleQueeneth <malukathembelihle95@gmail.com>
2026-02-20 11:43:49 +0200

    Feat: Working on the Register Modal

--- src/components/RegisterModal.tsx
+++ src/components/RegisterModal.tsx
@@ -1,6 +1,25 @@
+import { useAuth0 } from "@auth0/auth0-react";
 
-export const RegisterModal = () => {
+const RegisterModal = () => {
+  const { loginWithRedirect } = useAuth0();
+
+  const handleSignup = async () => {
+    await loginWithRedirect({
+      authorizationParams: {
+        screen_hint: "signup",
+        redirect_uri: window.location.origin + "/dashboard",
+      },
+    });
+  };
+
   return (
-    <div>RegisterModal</div>
-  )
-}
+    <button
+      onClick={handleSignup}
+      className="w-full py-3 rounded-xl bg-green-500 text-black font-semibold"
+    >
+      Create Account
+    </button>
+  );
+};
+
+export default RegisterModal;
