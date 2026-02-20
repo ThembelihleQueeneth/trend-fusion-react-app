d86d0929d7652059ac682dfbb537d920e5168c3d
ThembelihleQueeneth <malukathembelihle95@gmail.com>
2026-02-20 11:41:47 +0200

    Feat: Added a ProtectedDashboard to the App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,18 @@ import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import { Home } from './pages/Home'
 import { Dashboard } from './pages/Dashboard'
+import { withAuthenticationRequired } from '@auth0/auth0-react'
+
+const ProtectedDashboard = withAuthenticationRequired(Dashboard, {
+  onRedirecting: () => <div className='text-white p-10'>Loading...</div>
+})
 
 function App() {
 
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/dashboard" element={<ProtectedDashboard />} />
     </Routes>
   )
 }
